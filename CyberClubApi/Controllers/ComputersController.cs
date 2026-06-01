73dc5968f21701b6fc654744c8625bd2e1a6a736
levikroman <levikroman@gmail.com>
2026-06-01 18:48:08 +0300

    Реалізовано ComputersController з CRUD операціями (Етап 2.2)

---
+++ CyberClubApi/Controllers/ComputersController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CyberClubApi.Data;
+using CyberClubApi.Models;
+
+namespace CyberClubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ComputersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // 1. READ ALL 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Computer>>> GetComputers()
+        {
+            return await _context.Computers.ToListAsync();
+        }
+
+        // 2. READ ONE 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Computer>> GetComputer(int id)
+        {
+            var computer = await _context.Computers.FindAsync(id);
+
+            if (computer == null)
+            {
+                return NotFound();
+            }
+
+            return computer;
+        }
+
+        // 3. CREATE 
+        [HttpPost]
+        public async Task<ActionResult<Computer>> PostComputer(Computer computer)
+        {
+            _context.Computers.Add(computer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetComputer), new { id = computer.Id }, computer);
+        }
+
+        // 4. UPDATE 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComputer(int id, Computer computer)
+        {
+            if (id != computer.Id)
+            {
+                return BadRequest(); 
+            }
+
+            _context.Entry(computer).State = EntityState.Modified; 
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComputerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // 5. DELETE 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComputer(int id)
+        {
+            var computer = await _context.Computers.FindAsync(id);
+            if (computer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Computers.Remove(computer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        private bool ComputerExists(int id)
+        {
+            return _context.Computers.Any(e => e.Id == id);
+        }
+    }
+}
